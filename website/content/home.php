821095310709cc974b8fe48a41a8b3a4832df9d1
Churchil-Raja-a93 <churchilraja.a93@gmail.com>
2025-06-19 23:06:00 +0800

    Update home.php

--- website/content/home.php
+++ website/content/home.php
@@ -1,6 +1,6 @@
 <p>This is <b>home</b> Welcome to test project -- this content is in file -- content/home.php
 Feel free to edit it and check in git to test the CI/CD flow</p> 
-<p>Docker Pipeline</p>
+<p>Docker Pipeline check</p>
 </p>It is a long established fact that no one reads this and I may add anything and reader will be distracted by the 
 readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution 
 of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and 
